29f3ecccc7c943f056656d488d7363812450c6ba
JustinGod <33948903+JustinGod@users.noreply.github.com>
2018-04-04 13:53:27 -0400

    Fournisseur[.h/.cpp] fini

--- TP5/Fournisseur.h
+++ TP5/Fournisseur.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include "Usager.h"
 #include "Produit.h"
+#include "GestionnaireProduits.h"
 
 using namespace std;
 
@@ -14,21 +15,18 @@ class Fournisseur : public Usager
     Fournisseur();
     Fournisseur(const string &nom, const string &prenom, int identifiant, const string &codePostal);
 
-	// TODO : Modifier l'implémentation de ses méthodes : ----
-    vector<Produit *> obtenirCatalogue() const;
+	GestionnaireProduits* obtenirCatalogue() const;
     void afficherCatalogue() const;
     virtual void afficher() const;
     virtual void reinitialiser();
     virtual void ajouterProduit(Produit *produit);
     virtual void enleverProduit(Produit *produit);   
-	// -------------------------------------------------------
 
-	// TODO : implémenter les méthodes suivantes :
 	Produit* trouverProduitPlusCher() const;
 	void DiminuerPrix(int pourcent) const;
 
   private:
-    vector<Produit *> catalogue_;
+    GestionnaireProduits* catalogue_;
 };
 
 #endif
