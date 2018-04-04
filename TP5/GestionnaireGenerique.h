11eb607094b7dd3b3bacc9f28413ca2cf0e85077
JustinGod <33948903+JustinGod@users.noreply.github.com>
2018-04-04 16:46:34 -0400

    Arrange gestionnairegenerique

--- TP5/GestionnaireGenerique.h
+++ TP5/GestionnaireGenerique.h
@@ -11,56 +11,30 @@ template <typename T, typename C, typename A, typename S >
 class GestionnaireGenerique
 {
 public:
-	C obtenirConteneur() const;
-	void Ajouter(T* T);
-	void Supprimer(T* T);
-	template<typename Predicate>
-	void pourChaqueElement(Predicate predicate)const;
+	C obtenirConteneur() const
+	{
+		return conteneur_;
+	};
+
+	void Ajouter(T* t)
+	{
+		A ajouter(conteneur_);
+		conteneur_ = ajouter(t);
+	}
+
+	void Supprimer(T* t)
+	{
+		S supprimer(conteneur_);
+		conteneur_ = supprimer(t);
+	}
+
+	template<typename Foncteur>
+	void pourChaqueElement(Foncteur foncteur) const
+	{
+		for_each(conteneur_.begin(), conteneur_.end(), foncteur);
+	}
 
 
 protected:
 	C conteneur_;
 };
-
-	
-	
-	template <typename T, typename C, typename A, typename S >
-	void GestionnaireGenerique<T, C , A, S>::Ajouter (T* t)
-	{
-		A ajouter(conteneur_);
-			conteneur_ = ajouter(t);
-		
-
-	};
-
-	template <typename T, typename C, typename A, typename S >
-	void GestionnaireGenerique<T, C, A, S>::Supprimer (T* t)
-	{
-		S supprimer(conteneur_);
-		conteneur_ = supprimer(t);
-
-	};
-
-	template <typename T, typename C, typename A, typename S >
-	C GestionnaireGenerique<T, C, A, S>::obtenirConteneur() const
-	{
-		return conteneur_;
-	};
-
-	template <typename T, typename C, typename A, typename S >
-	void GestionnaireGenerique<T, C, A, S>::pourChaqueElement(Predicate predicate) const
-	{
-		for_each(conteneur_.begin(), conteneur_.end(), predicate);
-
-	};
-	
-
-
-// TODO : Méthodes :
-/*
-- ajouter()
-- supprime()
-- obtenirConteneur()
-- pourChaqueElement()
-*/
-
