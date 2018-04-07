00778d066640cbfb7d236d1ab11b6cb35431a995
JustinGod <33948903+JustinGod@users.noreply.github.com>
2018-04-06 21:44:09 -0400

    Fix ClientPremium.cpp

--- TP5/ClientPremium.cpp
+++ TP5/ClientPremium.cpp
@@ -27,12 +27,7 @@ unsigned int ClientPremium::obtenirJoursRestants() const
 
 double ClientPremium::obtenirTotalAPayer() const
 {
-    double montant = 0;
-    for (unsigned int i = 0; i < panier_.size(); i++) {
-        double prix = panier_->obtenirPrix();
-        montant += prix < 5 ? 0 : prix - 5;
-    }
-    return montant;
+	return panier_->obtenirTotalAPayer();
 }
 
 void ClientPremium::afficherProfil() const
